253939dcbd933a36bb17a95c501f9efba05beb80
J.D. Montgomery <monty0018@gmail.com>
2014-12-11 13:01:31 -0600

    added google maps to job detail page

--- Angular/app/job/jobDetail.js
+++ Angular/app/job/jobDetail.js
@@ -13,18 +13,45 @@
 			logError = getLogFn('jobDetail', 'error'),
 			vm = this;
 
+		vm.descriptionButtonText = 'Show Description';
 		vm.hasChanges = hasChanges;
 		vm.job = undefined;
+		vm.map = {
+		  zoom: 11,
+		  mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		vm.map.options = { scrollwheel: false };
 		vm.message = undefined;
 		vm.save = save;
+		vm.showHideDescription = showHideDescription;
 
 		activate();
 
 		function activate() {
 			common.activateController([getJob($routeParams.jobId)], 'jobDetail').
 				then(function () {
+					if (vm.job.Company.AddressOne)
+						codeAddress(vm.job.Company.AddressOne + ' ' + vm.job.Company.AddressTwo);
+
 					log('Activated Job Detail View');
 				});
+		}
+
+		function codeAddress(address) {
+			var geocoder = new google.maps.Geocoder();
+			geocoder.geocode({
+				'address': address
+			}, function (results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					vm.map.center = results[0].geometry.location;
+					var map = new google.maps.Map(document.getElementById("googleMap"), vm.map);
+
+					var marker = new google.maps.Marker({
+						map: map,
+						position: results[0].geometry.location
+					});
+				}
+			});
 		}
 
 		function getJob(id) {
@@ -55,6 +82,13 @@
 					logError('Failed to save changes.');
 				}
 			});
+		}
+
+		function showHideDescription() {
+			if (vm.descriptionButtonText == 'Show Description')
+				vm.descriptionButtonText = 'Hide Description';
+			else
+				vm.descriptionButtonText = 'Show Description';
 		}
 	}
 })();
